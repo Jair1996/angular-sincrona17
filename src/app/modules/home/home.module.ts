25432475641124b7a1ac33f30a74b3ae49b415fd
Jair Deyner <jairdeyner730791@gmail.com>
2023-01-12 12:13:58 -0500

    HomePage mdified

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
 
 
 @NgModule({
   declarations: [
-    HomePageComponent
+    HomePageComponent,
+    CarouselComponent
   ],
   imports: [
     CommonModule,
